b56f884e648af8b1c23cab487998a6ca15a535f6
gabrielpercabral <gabriel.cabrak5@gmail.com>
2025-06-16 19:06:17 -0300

    Criação do setup e dos APPs do projeto, além das URLs criadas em cada APP

---
+++ PeConfortavelLtda/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for PeConfortavelLtda project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PeConfortavelLtda.settings')
+
+application = get_asgi_application()
